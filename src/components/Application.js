c0f6f0df8365f7d3e2a12f52e1dd1a57fa6b082c
Nou <nou@MacBook-Pro.local>
2022-01-20 20:34:29 -0500

    combined states

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,9 +44,15 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
-
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
+  
   useEffect(() => {
     const dayURL = `http://localhost:8001/api/days`;
     axios.get(dayURL).then(response => {
@@ -66,9 +72,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
-            onChange={setDay}
+            days={state.days}
+            day={state.day}
+            setDay={setDay}
           />
         </nav>
         <img
