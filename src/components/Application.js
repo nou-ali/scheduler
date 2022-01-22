96b060f5693ccf0baf9ef81a883014700c9a354e
Nou <nou@MacBook-Pro.local>
2022-01-22 15:44:06 -0500

    adding mode constants

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,18 +1,32 @@
 import React, { useState, useEffect } from "react";
-import getAppointmentsForDay from "helpers/selectors";
+import {getAppointmentsForDay,getInterview} from "helpers/selectors";
 import axios from "axios";
 import DayList from "components/DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
 
+
+
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
-  const dailyAppointments =  getAppointmentsForDay(state, state.day);
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   const setDay = day => setState({ ...state, day });
 
@@ -20,13 +34,12 @@ export default function Application(props) {
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
       axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers'),
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
 
-
-  const appointmentData = dailyAppointments.map(appointment => <Appointment key={appointment.id} {...appointment} />);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -50,7 +63,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointmentData}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
