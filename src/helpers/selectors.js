e3632b44e689b975d5b56ea4209f0ce7f26ff6c7
Nou <nou@MacBook-Pro.local>
2022-01-21 19:25:32 -0500

    finally got interview data to work in app

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,19 @@
-export default function getAppointmentsForDay(state, day) {
+function getAppointmentsForDay(state, day) {
   const filteredDay = state.days.find((currentDay) => currentDay.name === day);
   const filteredAppointment = filteredDay ? filteredDay.appointments.map((id) => state.appointments[id]) : [];
   return filteredAppointment;
 }
+
+function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewData = state.interviewers[interview.interviewer];
+  return {
+    student: interview.student,
+    interviewer: interviewData
+  }
+}
+
+
+module.exports = {getAppointmentsForDay, getInterview}
