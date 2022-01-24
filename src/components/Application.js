00dc29d630c9088f8948fcf10fda28eda0afabec
Nou <nou@MacBook-Pro.local>
2022-01-24 16:29:29 -0500

    workoing on deleting function

--- src/components/Application.js
+++ src/components/Application.js
@@ -34,8 +34,24 @@ export default function Application(props) {
       })
   }
 
+  function cancelInterview (id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
 
-
+    return axios.delete(`/api/appointments/${id}`)
+      .then(res => {
+        setState({
+          ...state,
+          appointments
+        });
+      })
+  }
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
@@ -49,6 +65,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Status from "components/Appointment/Status";
 import Form from "components/Appointment/Form";
+import Confirm from "components/Appointment/Confirm";
 import useVisualMode from "hooks/useVisualMode";
 
 
@@ -12,6 +13,9 @@ const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
+
 
 export default function Appointment(props) {
 
@@ -35,6 +39,17 @@ export default function Appointment(props) {
       
   }
 
+  function deleteInterview() {
+    transition(DELETING);
+    props
+      .cancelInterview(props.id)
+      .then(() => transition(EMPTY)) 
+      .catch((err) => {
+        console.log(err)
+      });
+      
+  }
+
   return (
     <article
       className="appointment">
@@ -44,9 +59,15 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={() => transition(CONFIRM)}
         />
       )}
       {mode === SAVING && <Status message="Saving..."/>}
+      {mode === DELETING && <Status message="Deleting..."/>}
+      {mode === CONFIRM && <Confirm message="Are you sure you would like to delete this appointment?"
+          onConfirm={deleteInterview}
+          onCancel={back}
+      />}
       {mode === CREATE && (
         <Form
           name={props.name}
