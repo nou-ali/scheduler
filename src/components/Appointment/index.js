e078a3e3567f685d0f4c48c2a9300aa76891a086
Nou <nou@MacBook-Pro.local>
2022-01-24 20:55:37 -0500

    error handling

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,6 +6,7 @@ import Show from "components/Appointment/Show";
 import Status from "components/Appointment/Status";
 import Form from "components/Appointment/Form";
 import Confirm from "components/Appointment/Confirm";
+import Error from "components/Appointment/Error";
 import useVisualMode from "hooks/useVisualMode";
 
 
@@ -16,6 +17,8 @@ const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
 const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 
 export default function Appointment(props) {
@@ -34,18 +37,18 @@ export default function Appointment(props) {
       .bookInterview(props.id, interview)
       .then(() => transition(SHOW))
       .catch((err) => {
-        console.log(err)
+        transition(ERROR_SAVE, true)
       });
 
   }
 
   function deleteInterview() {
-    transition(DELETING);
+    transition(DELETING, true);
     props
       .cancelInterview(props.id)
       .then(() => transition(EMPTY))
       .catch((err) => {
-        console.log(err)
+        transition(ERROR_DELETE, true)
       });
 
   }
@@ -78,13 +81,26 @@ export default function Appointment(props) {
         />
       )}
 
-      {mode === EDIT && 
+      {mode === EDIT &&
         <Form
           student={props.interview.student}
           value={props.interview.interviewer.id}
           interviewers={props.interviewers}
           onSave={save}
           onCancel={() => back()}
+        />
+      }
+
+      {mode === ERROR_SAVE &&
+        <Error message="Could not save appointment"
+          onClose={() => back()}
+        />
+      }
+
+      {mode === ERROR_DELETE &&
+        <Error 
+        message="Could not delete appointment"
+        onClose={() => back()}
         />
       }
 
